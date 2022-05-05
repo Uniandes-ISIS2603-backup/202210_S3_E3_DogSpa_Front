32d3030f34691061df668006431eec78b9c5fdee
Paolaaaa <p.campino@uniandes.edu.co>
2022-05-05 10:26:22 -0500

    filtro servicio por sede
    >hay que revisar como borrar el detail

--- src/app/servicio/servicio-list/servicio-list.component.ts
+++ src/app/servicio/servicio-list/servicio-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Servicio } from '../servicio';
 import { ServicioService } from '../servicio.service';
 import { ServicioDetail } from '../servicio-detail';
+import { SedeService } from 'src/app/sede/sede.service';
+import { Sede } from 'src/app/sede/sede';
 
 @Component({
   selector: 'app-servicio-list',
@@ -13,8 +15,10 @@ export class ServicioListComponent implements OnInit {
   servicios: Array<ServicioDetail> = [];
   selected: Boolean = false;
   selectedService!: ServicioDetail;
+  sedes: Array<Sede>=[];
+  sede: string ='1';
 
-  constructor(private servicioService: ServicioService) { }
+  constructor(private servicioService: ServicioService, private sedeService:SedeService, private serviceSede:ServicioService) { }
 
   getServicios(): void{
     this.servicioService.getServicios().subscribe({next: servicios =>
@@ -27,7 +31,43 @@ export class ServicioListComponent implements OnInit {
     this.selectedService = servicio;
   }
 
+  getSedes():void
+
+  {
+    this.sedeService.getSedes().subscribe((sedes)=>
+    {
+      this.sedes =sedes;
+
+
+    });
+
+  }
+
+
+  getServicioenSede(idSede:string): void
+{
+  this.servicioService.getServicioenSede(idSede).subscribe((servicios)=>
+  {
+    this.servicios = servicios;
+  });
+}
+
+
   ngOnInit() {
-    this.getServicios();
+    //this.getServicios();
+    this.getSedes();
+    this.getServicioenSede(this.sede);
+
+  }
+
+  setSede():void
+  {
+    let value = (<HTMLSelectElement> document.getElementById('sede-select-service')).value;
+    this.sede= value;
+    console.log(this.sede) ;
+    this.getServicioenSede(value);
+
+
+
   }
 }
