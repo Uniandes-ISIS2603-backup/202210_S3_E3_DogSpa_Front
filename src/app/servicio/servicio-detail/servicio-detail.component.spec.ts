a16a9694582434fab3d1640397d2f368d6394ad7
juanyepesp <j.yepes@uniandes.edu.co>
2022-05-09 13:08:13 -0500

    pruebas implementadas

--- src/app/servicio/servicio-detail/servicio-detail.component.spec.ts
+++ src/app/servicio/servicio-detail/servicio-detail.component.spec.ts
@@ -1,28 +1,52 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-// import { ServicioDetailComponent } from './servicio-detail.component';
+import { ServicioDetailComponent } from './servicio-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { faker } from '@faker-js/faker';
+import { ServicioDetail } from '../servicio-detail';
 
-// describe('ServicioDetailComponent', () => {
-//   let component: ServicioDetailComponent;
-//   let fixture: ComponentFixture<ServicioDetailComponent>;
+describe('ServicioDetailComponent', () => {
+  let component: ServicioDetailComponent;
+  let fixture: ComponentFixture<ServicioDetailComponent>;
+  let debug: DebugElement;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ServicioDetailComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule, RouterTestingModule],
+      declarations: [ ServicioDetailComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ServicioDetailComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServicioDetailComponent);
+    component = fixture.componentInstance;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    component.servicioDetail = new ServicioDetail (
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.datatype.boolean(),
+      faker.image.imageUrl()
+    );
+
+    fixture.detectChanges();
+    debug = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have an img element', () => {
+    expect(debug.query(By.css('img')).attributes['alt']).toEqual(
+      component.servicioDetail.nombre
+    );
+  });
+});
