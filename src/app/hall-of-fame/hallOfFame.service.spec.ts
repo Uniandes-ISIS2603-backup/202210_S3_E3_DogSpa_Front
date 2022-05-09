eab56e65db943770ca5482e3c33620a0947d0ec2
juanyepesp <j.yepes@uniandes.edu.co>
2022-05-09 13:29:13 -0500

    pruebas implementadas

--- src/app/hall-of-fame/hallOfFame.service.spec.ts
+++ src/app/hall-of-fame/hallOfFame.service.spec.ts
@@ -1,18 +1,18 @@
-// /* tslint:disable:no-unused-variable */
+/* tslint:disable:no-unused-variable */
 
-// import { TestBed, async, inject } from '@angular/core/testing';
-// import { HallOfFameService } from '../hall-of-fame/hallOfFame.service';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HallOfFameService } from '../hall-of-fame/hallOfFame.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// describe('Service: Book', () => {
-//  beforeEach(() => {
-//    TestBed.configureTestingModule({
-//      imports: [HttpClientTestingModule],
-//      providers: [HallOfFameService]
-//    });
-//  });
+describe('Service: Book', () => {
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientTestingModule],
+     providers: [HallOfFameService]
+   });
+ });
 
-//  it('should ...', inject([HallOfFameService], (service: HallOfFameService) => {
-//    expect(service).toBeTruthy();
-//  }));
-// });
+ it('should ...', inject([HallOfFameService], (service: HallOfFameService) => {
+   expect(service).toBeTruthy();
+ }));
+});
