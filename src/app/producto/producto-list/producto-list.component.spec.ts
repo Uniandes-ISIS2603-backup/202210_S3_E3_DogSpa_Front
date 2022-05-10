87528d4e564ffcdef22279d0463f5265cc56dd79
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2022-05-10 17:22:25 -0500

    pruebas de sede-lista y sede-detail

--- src/app/producto/producto-list/producto-list.component.spec.ts
+++ src/app/producto/producto-list/producto-list.component.spec.ts
@@ -1,28 +1,37 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
-// import { ProductoListComponent } from './producto-list.component';
+import { ProductoListComponent } from './producto-list.component';
+import { Sede } from 'src/app/sede/sede';
 
-// describe('ProductoListComponent', () => {
-//   let component: ProductoListComponent;
-//   let fixture: ComponentFixture<ProductoListComponent>;
+describe('ProductoListComponent', () => {
+  let component: ProductoListComponent;
+  let fixture: ComponentFixture<ProductoListComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ProductoListComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductoListComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ProductoListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductoListComponent);
+    component = fixture.componentInstance;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    component.sede = new Sede(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence()
+    );
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/sede/sede-list/sede-list.component.spec.ts
+++ src/app/sede/sede-list/sede-list.component.spec.ts
@@ -1,28 +1,46 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
-// import { SedeListComponent } from './sede-list.component';
+import { SedeListComponent } from './sede-list.component';
+import { Sede } from '../sede';
 
-// describe('SedeListComponent', () => {
-//   let component: SedeListComponent;
-//   let fixture: ComponentFixture<SedeListComponent>;
+describe('SedeListComponent', () => {
+  let component: SedeListComponent;
+  let fixture: ComponentFixture<SedeListComponent>;
+  let debug: DebugElement;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SedeListComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SedeListComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SedeListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SedeListComponent);
+    component = fixture.componentInstance;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    component.sedes = [
+      new Sede(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+
+      ),
+    ];
+
+    fixture.detectChanges();
+    debug = fixture.debugElement;
+
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+});
