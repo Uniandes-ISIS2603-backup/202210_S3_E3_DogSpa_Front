43c7f2fc963adf8588b73e7101577cbe2754295d
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2022-05-10 08:03:13 -0500

    esto no funciona

--- src/app/sede/sede-detail/sede-detail.component.spec.ts
+++ src/app/sede/sede-detail/sede-detail.component.spec.ts
@@ -2,12 +2,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { SedeDetailComponent } from './sede-detail.component';
+import { Sede } from '../sede';
+import { SedeDetail } from '../sede-detail';
 
 describe('SedeDetailComponent', () => {
   let component: SedeDetailComponent;
   let fixture: ComponentFixture<SedeDetailComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,7 +23,17 @@ describe('SedeDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SedeDetailComponent);
     component = fixture.componentInstance;
+
+    component.sedeDetail = new SedeDetail(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+    );
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
--- src/app/sede/sede-list/sede-list.component.spec.ts
+++ src/app/sede/sede-list/sede-list.component.spec.ts
@@ -2,12 +2,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { SedeListComponent } from './sede-list.component';
+import { Sede } from '../sede';
 
 describe('SedeListComponent', () => {
   let component: SedeListComponent;
   let fixture: ComponentFixture<SedeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,7 +22,20 @@ describe('SedeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SedeListComponent);
     component = fixture.componentInstance;
+
+    component.sedes = [
+      new Sede(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      ),
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
+
   });
 
   it('should create', () => {
