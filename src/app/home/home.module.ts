2d22271310f6f7ffec370358dead74e8f015b0ce
juanyepesp <j.yepes@uniandes.edu.co>
2022-05-15 21:27:13 -0500

    implementación de home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SedeModule } from '../sede/sede.module';
+import { SedeListComponent } from '../sede/sede-list/sede-list.component';
+import { SedeDetailComponent } from '../sede/sede-detail/sede-detail.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SedeModule
   ],
+  providers: [],
   declarations: [HomeComponent]
 })
 export class HomeModule { }
