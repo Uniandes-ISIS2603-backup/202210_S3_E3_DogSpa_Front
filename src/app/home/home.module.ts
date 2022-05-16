0283a07516608ae4c7eeec19c662045795b32979
juanyepesp <j.yepes@uniandes.edu.co>
2022-05-16 18:41:13 -0500

    Arreglos de espaciado

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SedeModule } from '../sede/sede.module';
-import { SedeListComponent } from '../sede/sede-list/sede-list.component';
-import { SedeDetailComponent } from '../sede/sede-detail/sede-detail.component';
 
 @NgModule({
   imports: [
