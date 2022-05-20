a3430faf4969098f55522684a2a70c8ce0a63afa
davidvaldh <ds.valderrama@uniandes.edu.co>
2022-05-20 13:09:02 -0500

    Supresión de HallOfFame de navbar y arreglo de routeo

--- src/app/hall-of-fame/hall-of-fame.module.ts
+++ src/app/hall-of-fame/hall-of-fame.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HallOfFameListComponent } from './hall-of-fame-list/hall-of-fame-list.component';
 import { HallOfFameGalleryComponent } from './hall-of-fame-gallery/hall-of-fame-gallery.component';
 import { HallOfFameCarouselComponent } from './hall-of-fame-carousel/hall-of-fame-carousel.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HallOfFameRoutingModule } from './hall-of-fame-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
-    HallOfFameRoutingModule
   ],
-  declarations: [HallOfFameListComponent, HallOfFameGalleryComponent, HallOfFameCarouselComponent],
-  exports: [HallOfFameListComponent, HallOfFameGalleryComponent, HallOfFameCarouselComponent]
+  declarations: [HallOfFameGalleryComponent, HallOfFameCarouselComponent],
+  exports: [HallOfFameGalleryComponent, HallOfFameCarouselComponent]
 })
 export class HallOfFameModule { }
